381b2f08dc7bc669c2aa4af3b79b4af87793b34a
TechVeerA <techveera007@gmail.com>
2015-10-08 12:55:28 +0000

    to-ify
    Change-Id: Ibbfc42f133607a092dac201a635776dddffe3c12

---
+++ device/moto/falcon/to.mk
@@ -0,0 +1,7 @@
+
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+# Inherit some common TO stuff.
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := to_falcon
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
